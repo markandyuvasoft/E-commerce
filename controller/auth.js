edfd225e38cf96f0c31c2356b5674380dcd58212
markandyuvasoft <markandyuvasoft408@gmail.com>
2023-03-01 02:53:23 -0800

    add same

--- controller/auth.js
+++ controller/auth.js
@@ -39,24 +39,67 @@ const authRegister = async (req, res, next) => {
 };
 
 // login api....
+// const authLogin = async (req, res, next) => {
+//   const { email, password } = req.body;
+
+//   if (!email || !password) {
+//     res.status(400).send({ error: "please fill the proper field " });
+
+//   } else {
+//     let user = await User.findOne({ email: req.body.email });
+
+//     if (!user) {
+//       return res.status(404).send({ error: "invalid email" });
+
+//     } else if (user.isVarified === 0) {
+
+//       res.status(400).send({ error: "not allow by admin" })
+//     }
+
+//     else {
+      // const checkpassword = await bcrypt.compare(
+      //   req.body.password,
+      //   user.password
+      // );
+
+      // if (!checkpassword) {
+      //   return res.status(404).send({ error: "invalid password" });
+      // }
+//       const token = await createtoken(user._id);
+
+//       // console.log(token);
+
+//       let Id = user._id;
+
+//       res.status(200).send({ success: "😉welcome..!!", token, Id });
+//     }
+//   }
+// };
+
 const authLogin = async (req, res, next) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    res.status(400).send({ error: "please fill the proper field " });
+    if (!email || !password) {
+      res.status(400).send({ error: "please fill the proper field " });
     
-  } else {
-    let user = await User.findOne({ email: req.body.email });
+    } else {
+      let user = await User.findOne({ email: req.body.email });
 
-    if (!user) {
-      return res.status(404).send({ error: "invalid email" });
-    
-    } else if (user.isVarified === 0) {
-
-      res.status(400).send({ error: "not allow by admin" })
-    }
-    else {
-      const checkpassword = await bcrypt.compare(
+      if (!user) {
+        return res.status(404).send({ error: "invalid email" });
+      
+      }
+      if (user.isAdmin === false) {
+        const token = await createtoken(user._id);
+        user.token = token;
+        const login = await user.save();
+        let Id = user._id;
+        return res.status(200).json({ success: "Welcome user..!!", token, Id });
+      }
+      
+       else {
+        const checkpassword = await bcrypt.compare(
         req.body.password,
         user.password
       );
@@ -64,14 +107,16 @@ const authLogin = async (req, res, next) => {
       if (!checkpassword) {
         return res.status(404).send({ error: "invalid password" });
       }
-      const token = await createtoken(user._id);
-
-      // console.log(token);
-
-      let Id = user._id;
-
-      res.status(200).send({ success: "😉welcome..!!", token, Id });
+        const token = await createtoken(user._id);
+        user.token = token;
+        const login = await user.save();
+        let Id = user._id;
+        res.status(200).json({ success: "Welcome admin..!!", token, Id });
+      
+      }
     }
+  } catch (error) {
+    res.status(401).json({ message: error.message });
   }
 };
 
@@ -106,12 +151,14 @@ const changePassword = async (req, res) => {
 // get user profile
 const getUser = async (req, res) => {
   try {
-    const details = await User.find({ _id: req.user._id }).select('-password -cPassword -isAdmin')
+    const details = await User.find({ _id: req.user._id }).select(
+      "-password -cPassword -isAdmin"
+    );
 
     if (!details) {
       res.status(400).send({ error: "not found user detail" });
     } else {
-      res.status(200).send({user:details});
+      res.status(200).send({ user: details });
     }
   } catch (error) {
     res.status(400).send(error);
@@ -119,105 +166,91 @@ const getUser = async (req, res) => {
 };
 
 // get user by id
-const getUserById = async (req,res) =>{
-
+const getUserById = async (req, res) => {
   try {
+    const id = req.params.id;
 
-    const id= req.params.id
+    const detail = await User.find({ _id: req.user._id });
 
-    const detail = await User.find({ _id: req.user._id})
+    if (detail) {
+      const user = await User.findById(id).select(
+        "-password -cPassword -isAdmin"
+      );
 
-    if(detail) {
-      const user= await User.findById(id).select('-password -cPassword -isAdmin')
-
-      res.status(200).send(user)
-    }else{
-      res.status(400).send({message:"user not found"})
+      res.status(200).send(user);
+    } else {
+      res.status(400).send({ message: "user not found" });
     }
-    
   } catch (error) {
     res.status(400).send(error);
-
   }
-}
+};
 
 // update user profile
-const updateUser = async (req,res) =>{
-
+const updateUser = async (req, res) => {
   try {
-    const id= req.params.id
-    const detail = await User.find({ _id: req.user._id})
+    const id = req.params.id;
+    const detail = await User.find({ _id: req.user._id });
 
-    if(detail){
+    if (detail) {
+      const updateuser = await User.findByIdAndUpdate(id, req.body, {
+        new: true,
+      }).select("-password -isAdmin -cPassword");
 
-      const updateuser = await User.findByIdAndUpdate(id,req.body,{
-        new:true
-      }).select("-password -isAdmin -cPassword")
-
-      res.status(200).send({updatedUser:updateuser})
-    }else{
-      res.status(400).send({message:"user not found"})
-
+      res.status(200).send({ updatedUser: updateuser });
+    } else {
+      res.status(400).send({ message: "user not found" });
     }
-
-  }
-  catch(error) {
-
+  } catch (error) {
     res.status(400).send(error);
-
   }
-
-}
+};
 
 // delete user profile
-const deleteUser = async (req,res) =>{
-
+const deleteUser = async (req, res) => {
   try {
-    
-    const id = req.params.id
+    const id = req.params.id;
 
     const details = await User.find({ _id: req.user._id });
 
-    if(details) {
+    if (details) {
+      const user = await User.findByIdAndDelete(id);
 
-      const user = await User.findByIdAndDelete(id)
-
-      res.status(200).send({message:"user deleted"})
+      res.status(200).send({ message: "user deleted" });
+    } else {
+      res.status(200).send({ message: "no user found" });
     }
-    else {
-      res.status(200).send({message:"no user found"});
-    }
-
-
   } catch (error) {
-    
-    res.status(400).send(error)
+    res.status(400).send(error);
   }
-}
+};
 
 // post the comment on product
-const commentProduct = async (req,res) =>{
+const commentProduct = async (req, res) => {
   const comment = {
-    text:req.body.text,
-    postedby:req.user._id
-}
-Product.findByIdAndUpdate(req.body.productId,{
-    $push:{comments:comment}
-},{
-    new:true
-})
-.populate("comments.postedby","_id firstName")
-.populate("postedby","_id firstName")
-
-.exec((err,result)=>{
-    if(err){
-        return res.status(422).send({error:err})
-    }else{
-        res.status(200).send(result)
+    text: req.body.text,
+    postedby: req.user._id,
+  };
+  Product.findByIdAndUpdate(
+    req.body.productId,
+    {
+      $push: { comments: comment },
+    },
+    {
+      new: true,
     }
-})
-  
-}
+  )
+    .populate("comments.postedby", "_id firstName")
+    .populate("postedby", "_id firstName")
+
+    .exec((err, result) => {
+      if (err) {
+        return res.status(422).send({ error: err });
+      } else {
+        res.status(200).send(result);
+      }
+    });
+};
 
 export default {
   authController,
@@ -228,5 +261,5 @@ export default {
   updateUser,
   deleteUser,
   getUserById,
-  commentProduct
+  commentProduct,
 };
